ff7ec6d2fa2766e19c026ca86b6916ba84829b85
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-03-25 07:55:40 +0100

    ex5(ec4): add test for server 500 response
    - To test this unknown server error we can override our server handler
      by setting up a new one in this specific test using server.use

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import {render, waitForElementToBeRemoved, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
+import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import {handlers} from 'test/server-handlers'
 import Login from '../../components/login-submission'
@@ -47,4 +48,24 @@ test('omitting the password results in an error', async () => {
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+test('unexpected server error displays error message', async () => {
+  const testErrorMessage = 'Something went bad server-side'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+
+  render(<Login />)
+
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
