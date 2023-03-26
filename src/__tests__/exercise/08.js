e88a344ab0226801ea4abe7c899abd6cb095cf25
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-03-26 13:01:15 +0200

    ex8(ec1): update first test to use a fake component
    - Instead of using our useCounter example we create a fake component
      that doesn't render anything but gives us access to the functions in
      useCounter to interact with
    - This approach can be useful to take in cases were it's hard to create
      an example component for our test, especially to handle edge cases

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,7 +2,7 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
@@ -18,19 +18,17 @@ function UseCounterExample(props) {
 }
 
 test('exposes the count and increment/decrement functions', async () => {
-  render(<UseCounterExample />)
-
-  const increment = screen.getByRole('button', {name: /increment/i})
-  const decrement = screen.getByRole('button', {name: /decrement/i})
-  const message = screen.getByText(/current count/i)
-
-  expect(message).toHaveTextContent('Current count: 0')
-
-  await userEvent.click(increment)
-  expect(message).toHaveTextContent('Current count: 1')
-
-  await userEvent.click(decrement)
-  expect(message).toHaveTextContent('Current count: 0')
+  let result
+  function TestComponent() {
+    result = useCounter()
+    return null
+  }
+  render(<TestComponent />)
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
 })
 
 test('starts with specified initalCount', async () => {
