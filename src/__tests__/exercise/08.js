030724bc9008aeb1220380d9b12153937cef080a
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-03-26 13:18:18 +0200

    ex8(ec2): update the 2nd & 3rd test to use fake component method
    - Our fake component needs some refactoring to handle passing props
    - We break the fake component out into a setup function that we can use
      to get the result object in each test

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,28 +2,20 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen, act} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-function UseCounterExample(props) {
-  const {count, increment, decrement} = useCounter(props)
-  return (
-    <div>
-      <div>Current count: {count}</div>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Increment</button>
-    </div>
-  )
-}
-
-test('exposes the count and increment/decrement functions', async () => {
-  let result
+function setup(...args) {
+  const result = {}
   function TestComponent() {
-    result = useCounter()
+    Object.assign(result, useCounter(...args))
     return null
   }
   render(<TestComponent />)
+  return result
+}
+test('exposes the count and increment/decrement functions', async () => {
+  const result = setup()
   expect(result.count).toBe(0)
   act(() => result.increment())
   expect(result.count).toBe(1)
@@ -31,39 +23,24 @@ test('exposes the count and increment/decrement functions', async () => {
   expect(result.count).toBe(0)
 })
 
-test('starts with specified initalCount', async () => {
+test('allows customization of the initial count', async () => {
   const startCount = 3
-  render(<UseCounterExample initialCount={startCount} />)
-
-  const increment = screen.getByRole('button', {name: /increment/i})
-  const decrement = screen.getByRole('button', {name: /decrement/i})
-  const message = screen.getByText(/current count/i)
-
-  expect(message).toHaveTextContent(`Current count: ${startCount}`)
-
-  await userEvent.click(increment)
-  expect(message).toHaveTextContent(`Current count: ${startCount + 1}`)
-
-  await userEvent.click(decrement)
-  expect(message).toHaveTextContent(`Current count: ${startCount}`)
+  const result = setup({initialCount: startCount})
+  expect(result.count).toBe(startCount)
+  act(() => result.increment())
+  expect(result.count).toBe(startCount + 1)
+  act(() => result.decrement())
+  expect(result.count).toBe(startCount)
 })
 
-test('increments and decrements with specified step', async () => {
-  const startCount = 0
+test('allows customization of the step', async () => {
   const stepSize = 3
-  render(<UseCounterExample initialCount={startCount} step={stepSize} />)
-
-  const increment = screen.getByRole('button', {name: /increment/i})
-  const decrement = screen.getByRole('button', {name: /decrement/i})
-  const message = screen.getByText(/current count/i)
-
-  expect(message).toHaveTextContent(`Current count: ${startCount}`)
-
-  await userEvent.click(increment)
-  expect(message).toHaveTextContent(`Current count: ${startCount + stepSize}`)
-
-  await userEvent.click(decrement)
-  expect(message).toHaveTextContent(`Current count: ${startCount}`)
+  const result = setup({step: stepSize})
+  expect(result.count).toBe(0)
+  act(() => result.increment())
+  expect(result.count).toBe(stepSize)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
