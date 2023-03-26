c6847e33c852f78687c47bf95c701dfa4b7dde55
Jonas Kinnvall <jonaskinnvall+github@pm.me>
2023-03-26 13:34:29 +0200

    ex8(ec3): use renderHook from testing-library instead of our own setup
    - Testing Library has an API that does what we did with the setup
      function, so we'll use that instead to reduce some boilerplate
      - renderHook is mostly useful for people developing libraries with
        hook, otherwise you should usually test the component that uses the
        hook
    - Also add a test to showcase how we can call rerender from renderHook
      to change props in our test

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,46 +1,52 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
+import {act, renderHook} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-function setup(...args) {
-  const result = {}
-  function TestComponent() {
-    Object.assign(result, useCounter(...args))
-    return null
-  }
-  render(<TestComponent />)
-  return result
-}
 test('exposes the count and increment/decrement functions', async () => {
-  const result = setup()
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  const {result} = renderHook(useCounter)
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 test('allows customization of the initial count', async () => {
   const startCount = 3
-  const result = setup({initialCount: startCount})
-  expect(result.count).toBe(startCount)
-  act(() => result.increment())
-  expect(result.count).toBe(startCount + 1)
-  act(() => result.decrement())
-  expect(result.count).toBe(startCount)
+  const {result} = renderHook(useCounter, {
+    initialProps: {initialCount: startCount},
+  })
+  expect(result.current.count).toBe(startCount)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(startCount + 1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(startCount)
 })
 
 test('allows customization of the step', async () => {
   const stepSize = 3
-  const result = setup({step: stepSize})
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(stepSize)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  const {result} = renderHook(useCounter, {
+    initialProps: {step: stepSize},
+  })
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(stepSize)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+
+test('the step can be changed', () => {
+  const {result, rerender} = renderHook(useCounter, {
+    initialProps: {step: 3},
+  })
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(3)
+  rerender({step: 2})
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(1)
 })
 
 /* eslint no-unused-vars:0 */
